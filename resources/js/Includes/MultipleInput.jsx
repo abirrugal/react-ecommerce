81305dd743e583e5916e334301bb0e72bfdc4c2a
alaminse <abir.rugal@gmail.com>
2023-12-13 15:06:01 +0600

    Update design of the product edit page

--- resources/js/Includes/MultipleInput.jsx
+++ resources/js/Includes/MultipleInput.jsx
@@ -26,52 +26,52 @@ const MultipleInput = ({ onSizeDataChange, variants }) => {
   };
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {productVariant.map((variant, index) => (
-            <tr key={index}>
-              <td>
-                <select name="attributeName" value={variant.attributeName} class="form-select" id="category_id" onChange={(e) => handleInputChange(index, e)}>
-                  <option>Select Variant</option>
-                  {variants.map(({ name }) => {
-                    return (<option value={name.toLowerCase()}>{name}</option>)
-                  })}
-                </select>
-              </td>
-
-              <td>
-                <input
-                  type="text"
-                  name="attributeValue"
-                  className="form-control"
-                  placeholder="Variant Value Ex:(red, XL)"
-                  value={variant.attributeValue}
-                  onChange={(e) => handleInputChange(index, e)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  name="additionalPrice"
-                  className="form-control"
-                  placeholder="Additional Price"
-                  value={variant.additionalPrice}
-                  onChange={(e) => handleInputChange(index, e)}
-                />
-              </td>
-              <td>
-
-                <button type="button" className="btn btn-danger remove-table-row" onClick={() => handleRemoveSize(index)}>Remove</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button type="button" className='btn btn-primary remove-table-row mt-2' onClick={handleAddSize}>
-        Add
-      </button>
-    </div>
+<div style={{ width: '100%' }}>
+  <table style={{ width: '100%', tableLayout: 'fixed' }}>
+    <tbody>
+      {productVariant.map((variant, index) => (
+        <tr key={index}>
+          <td style={{ width: '30%' }}>
+            <select name="attributeName" value={variant.attributeName} className="form-select" onChange={(e) => handleInputChange(index, e)}>
+              <option>Select Variant</option>
+              {variants.map(({ name }) => (
+                <option key={name} value={name.toLowerCase()}>{name}</option>
+              ))}
+            </select>
+          </td>
+          <td style={{ width: '30%' }}>
+            <input
+              type="text"
+              name="attributeValue"
+              className="form-control"
+              placeholder="Variant Value Ex:(red, XL)"
+              value={variant.attributeValue}
+              onChange={(e) => handleInputChange(index, e)}
+            />
+          </td>
+          <td style={{ width: '30%' }}>
+            <input
+              type="text"
+              name="additionalPrice"
+              className="form-control"
+              placeholder="Additional Price"
+              value={variant.additionalPrice}
+              onChange={(e) => handleInputChange(index, e)}
+            />
+          </td>
+          <td style={{ width: '10%' }}>
+            <button type="button" className="btn btn-danger remove-table-row" onClick={() => handleRemoveSize(index)}>
+              Remove
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  <button type="button" className='btn btn-primary remove-table-row mt-2' onClick={handleAddSize}>
+    Add
+  </button>
+</div>
   );
 };
 
--- resources/js/Pages/Product/Edit.jsx
+++ resources/js/Pages/Product/Edit.jsx
@@ -77,8 +77,9 @@ const Create = ({ categories, brands, variants, product }) => {
           const imgElement = document.createElement('img');
           imgElement.src = event.target.result;
           imgElement.alt = 'Preview';
-          imgElement.style.maxWidth = '120px';
-          imgElement.style.marginRight = '1px';
+          imgElement.style.maxWidth = '100px';
+          imgElement.style.marginRight = '0px';
+          imgElement.style.marginTop = '8px';
           previewImg.appendChild(imgElement);
         };
         reader.readAsDataURL(image);
@@ -152,7 +153,7 @@ const Create = ({ categories, brands, variants, product }) => {
               </div>
             </div>
             <div class="row">
-              <div class="col-sm-12 mb-3">
+              <div class="col-sm-12 mb-2">
                 <label for="name" class="col-form-label">Product Name :</label>
                 <div class="form-group">
                   <input type="text" id="name" name="name" value={data.name} onChange={(e) => setData('name', e.target.value)} minlength="3" class="form-control" placeholder="Product Name" />
@@ -160,19 +161,19 @@ const Create = ({ categories, brands, variants, product }) => {
               </div>
             </div>
             <div class="row">
-              <div class="col-sm-4 mb-3">
+              <div class="col-sm-4 mb-2">
                 <label for="price" class="col-form-label">Product Price :</label>
                 <div class="form-group">
                   <input type="number" id="price" name="price" value={data.price} onChange={(e) => setData('price', e.target.value)} class="form-control" placeholder="Product Price" />
                 </div>
               </div>
-              <div class="col-sm-4 mb-3">
+              <div class="col-sm-4 mb-2">
                 <label for="discount" class="col-form-label">Product Discount (Ex: 5 for 5% discount):</label>
                 <div class="form-group">
                   <input type="number" id="discount" name="discount" value={data.discount} onChange={(e) => setData('discount', e.target.value)} class="form-control" placeholder="Product discount Price" />
                 </div>
               </div>
-              <div class="col-sm-4 mb-3">
+              <div class="col-sm-4 mb-2">
                 <label for="stock_in" class="col-form-label">Product Current Stock :</label>
                 <div class="form-group">
                   <input type="number" id="stock_in" name="stock_in" value={data.stock_in} onChange={(e) => setData('stock_in', e.target.value)} class="form-control" placeholder="Product Current Stock" />
@@ -180,7 +181,7 @@ const Create = ({ categories, brands, variants, product }) => {
               </div>
             </div>
             <div class="row">
-              <div class="col-sm-12 mb-3">
+              <div class="col-sm-12 mb-2">
                 <label for="description" class="col-form-label">Product Description :</label>
                 <div class="form-group">
                   <textarea class="form-control" id="description" value={data.description} name="description" onChange={(e) => setData('description', e.target.value)} placeholder="Product Description here" ></textarea>
@@ -193,11 +194,10 @@ const Create = ({ categories, brands, variants, product }) => {
 
             </div>
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col">
                 <label for="image" class="col-form-label">Product Image :</label>
                 <div class="form-group">
                   <input class="form-control" type="file" name="image" id="image" onChange={handleImage} />
-
                   {product.image && <img src={base_url + '/' + product.image}
                     alt="Admin" height="100px" id="mainImageShow" className='my-2' />}
                 </div>
@@ -207,13 +207,10 @@ const Create = ({ categories, brands, variants, product }) => {
                 <label className="form-check-label" htmlForfor="status">Status</label>
               </div>
               <input type="submit" class="btn btn-primary px-4 submit my-4" value="Update Product" />
-
               <hr />
-
             </div>
           </form>
         </div>
-
         <div className='row bg-white p-5 my-5'>
           <h4>Gallery Images</h4>
           <div class="col-12">            
@@ -223,21 +220,23 @@ const Create = ({ categories, brands, variants, product }) => {
                 <button class="btn btn-primary" onClick={uploadGalleryImages} type="button">Upload</button>
               </div>
             </div>
-            {/* <img src='' id='multiImagePreview' className='mt-3' width='150px' /> */}
             <div class="row" id="preview_img"></div>
           </div>
-
           <div class="row">
             {product.images && (product.images.map(({ id, image }) => {
-              return (<div class="col-md-3 col-xl-2">
-                <div class="card mt-3" style={{ width: '12rem' }}>
-                  <img class="card-img-top mr-3" src={base_url + '/' + image} alt="Card image cap" />
-                  <div class="card-body text-center py-1">
-                    {/* <h5 class="card-title">{image}</h5> */}
-                    <a href="#" onClick={(e) => removeGalleryImages(id)} class="link-danger">Remove</a>
-                  </div>
+              return (<div className="col-md-3 col-xl-2">
+              <div className="card mt-3" style={{ width: '12rem', position: 'relative' }}>
+                <button onClick={(e) => removeGalleryImages(id)}
+                  type="button"
+                  className="btn   btn-secondary"
+                  style={{ position: 'absolute', top: '5px', right: '5px', zIndex: '1' , backgroundColor:'rgba(0, 0, 0, 0.5)' }}
+                  aria-label="Close"
+                >X</button>
+                <img className="card-img-top mr-3" src={base_url + '/' + image} alt="Card image cap" />
+                <div className="card-body text-center py-0">
                 </div>
-              </div>)
+              </div>
+            </div>)
             }))
             }
           </div>
